7bf38a36aa4d73ec47cc595d159b345f004e02c9
jankowskij <volggalla@gmail.com>
2021-01-04 17:19:52 +0300

    UNK

--- components/Router.php
+++ components/Router.php
@@ -2,48 +2,70 @@
 
 class Router
 {
-    private $routes = [];
 
-    public function __construct() {
-        $routesPath = ROOT.'/config/routes.php';
+    private $routes;
+
+    public function __construct()
+    {
+        $routesPath = ROOT . '/config/routes.php';
         $this->routes = include($routesPath);
     }
 
-    public function getUri() {
+    /**
+     * Returns request string
+     */
+    private function getURI()
+    {
         if (!empty($_SERVER['REQUEST_URI'])) {
             return trim($_SERVER['REQUEST_URI'], '/');
         }
     }
 
-    public function run() {
-        $uri = $this->getUri();
-        foreach ($this->routes as $uriPattern => $path) { // Разбиваем массив с роутами построчно
-            if (preg_match("~$uriPattern~", $uri)){ // Ищем в браузерной строке совпадение с строками в роутере
-                $internalRout = preg_replace("~$uriPattern~", $path, $uri); // Нашли, производим замену переменных регуляркой
-                $segments = explode('/', $internalRout); // Сегментируем полученную строку, записываем в массив
-                $controllerName = array_shift($segments).'Controller'; // Получаем название контроллера (Класса)
-                $controllerName = ucfirst($controllerName); // Делаем первую букву заглавной, для корректности
-                $actionName = 'action'.ucfirst(array_shift($segments)); //  Получаем имя метода-action
-                $parameters = $segments; // Остаток строки записываем в массив (Параметры для action)
-                $controllerFile = ROOT . '/controllers/'.$controllerName.'.php'; // Формируем путь к контроллеру
+    public function run()
+    {
+        // Получить строку запроса
+        $uri = $this->getURI();
 
-            if (file_exists($controllerFile)) { // Подключаем файл контроллера
-                include_once($controllerFile);
-            }
+        // Проверить наличие такого запроса в routes.php
+        foreach ($this->routes as $uriPattern => $path) {
 
-            $controllerObject = new $controllerName; // Создаем объект класса контроллер
-            /*
-            Вызываем функцию, которая вызывает метод actionName
-            класса controllerObject, c переданным массивом параметров
-            $parameters
-            */   
-            $rezult = call_user_func_array(array($controllerObject, $actionName), $parameters);
-            if ($rezult!=null) {
-                break;
+            // Сравниваем $uriPattern и $uri
+            if (preg_match("~$uriPattern~", $uri)) {
+                
+                // Получаем внутренний путь из внешнего согласно правилу.
+                $internalRoute = preg_replace("~$uriPattern~", $path, $uri);
+                                
+                // Определить контроллер, action, параметры
+
+                $segments = explode('/', $internalRoute);
+
+                $controllerName = array_shift($segments) . 'Controller';
+                $controllerName = ucfirst($controllerName);
+
+                $actionName = 'action' . ucfirst(array_shift($segments));
+                             
+                $parameters = $segments;
+                
+                // Подключить файл класса-контроллера
+                $controllerFile = ROOT . '/controllers/' .
+                        $controllerName . '.php';
+
+                if (file_exists($controllerFile)) {
+                    include_once($controllerFile);
+                }
+
+                // Создать объект, вызвать метод (т.е. action)
+                $controllerObject = new $controllerName;
+                
+
+                $result = call_user_func_array(array($controllerObject, $actionName), $parameters);
+                
+                
+                if ($result != null) {
+                    break;
+                }
             }
         }
     }
-    }
-
 
 }
---
+++ config/db_params.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'host' => 'localhost',
+    'dbname' => 'phpshop',
+    'user' => 'user_1',
+    'password' => 'PASS!dkflbr4444',            
+);
